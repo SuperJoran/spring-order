e0a7705f483aa3946906342854ea23149fdb8113
Joran De Boever <Joran.deboever@esfds.com>
2017-12-07 21:43:02 +0100

    Person username unique constraint

---
+++ Web/src/main/resources/db/migration/V001_009__person_name_unique.sql
@@ -0,0 +1,5 @@
+-------------------------------------------------
+-- @author: jorandeboever
+-- @since: 7/12/17
+-------------------------------------------------
+ALTER TABLE SPR_PERSON ADD CONSTRAINT UN01_SPR_PERSON UNIQUE(USERNAME);
