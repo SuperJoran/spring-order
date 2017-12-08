239bfd4854f409a2d0edeeba1344702c449e102b
Joran De Boever <Joran.deboever@esfds.com>
2017-12-08 17:22:33 +0100

    currencyformatutility nullable

--- Web/src/main/java/be/jorandeboever/utilities/CurrencyFormatUtility.java
+++ Web/src/main/java/be/jorandeboever/utilities/CurrencyFormatUtility.java
@@ -2,6 +2,7 @@ package be.jorandeboever.utilities;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.util.Optional;
 
 public class CurrencyFormatUtility {
 
@@ -10,6 +11,6 @@ public class CurrencyFormatUtility {
 
     public static String formatAmount(BigDecimal amount) {
         //TODO why is this an american format
-        return NumberFormat.getCurrencyInstance().format(amount);
+        return Optional.ofNullable(amount).map(a -> NumberFormat.getCurrencyInstance().format(a)).orElse(null);
     }
 }
